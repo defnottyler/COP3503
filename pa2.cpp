3e7ec1d88f0402744fe2a2cbb677034eab24f360
tycanady <tycanady@gmail.com>
2016-10-23 23:21:26 -0400

    Update pa2.cpp

--- pa2.cpp
+++ pa2.cpp
@@ -1,9 +1,7 @@
 #include "pa2.h"
 
 //Default linked list constructor
-LinkedList::LinkedList() {
-	head = NULL;
-}
+
 
 int main(int argc, char *argv[]) {
 	// if user opts for best fit
@@ -68,140 +66,152 @@ void bestFit() {
 			}
 			//if there is used memory
 			else {
-				
-				
+				usedMemory.insertBest(programName);
 			}//end else
-
-			// add program to used memory
-			for (int i = 0; i < numPages; i++)
-				usedMemory.addProgram(programName);
-			//usedMemory.displayList();
-			break; //break switch
 		}
 		}
+
+
+
 	}
 
 
 
-}
 
+	void worstFit() {
+		LinkedList usedMemory;
+		LinkedList freeMemory;
+		cout << "Using worst fit algorithm" << endl;
+		displayMenu();
 
-void worstFit() {
-	LinkedList usedMemory;
-	LinkedList freeMemory;
-	cout << "Using worst fit algorithm" << endl;
-	displayMenu();
+		int choice;
+		cout << "choice - ";
+		cin >> choice;
 
-	int choice;
-	cout << "choice - ";
-	cin >> choice;
+		while (choice != 5) {
+			switch (choice) {
+				// Add Program
+			case 1:
+			{
+				string programName;
+				cout << "Program name - ";
+				cin >> programName;
 
-	while (choice != 5) {
-		switch (choice) {
-			// Add Program
-		case 1:
-		{
-			string programName;
-			cout << "Program name - ";
-			cin >> programName;
+				int programSize;
+				cout << "Program size - ";
+				cin >> programSize;
 
-			int programSize;
-			cout << "Program size - ";
-			cin >> programSize;
+				int numPages;
+				numPages = kbToPages(programSize);
 
-			int numPages;
-			numPages = kbToPages(programSize);
-
-			for (int i = 0; i < numPages; i++)
-				usedMemory.addProgram(programName);
-			//usedMemory.displayList();
+				for (int i = 0; i < numPages; i++)
+					usedMemory.addProgram(programName);
+				//usedMemory.displayList();
+			}
+			}
 		}
+
+
+
+	}
+
+	int LinkedList::kbToPages(int programSize) {
+		//4 kb's per page
+		int pages = programSize / 4;
+
+		//If program size isn't divisible by 4, must add extra page
+		if ((programSize % 4) > 0)
+			pages = pages + 1;
+
+		return pages;
+	}
+
+	void LinkedList::addProgram(string programName) {
+
+		//Check if head is null
+		//if it is, create "temporary" page
+		//Assign head values through this page and return
+		if (head == NULL) {
+			page *temp = new page;
+			temp->programName = programName;
+			temp->nextPage = NULL;
+			head = temp;
+			return;
 		}
-	}
+		//Create page to traverse through list, starting at head
+		page *current = head;
 
-
-
-}
-
-int kbToPages(int programSize) {
-	//4 kb's per page
-	int pages = programSize / 4;
-
-	//If program size isn't divisible by 4, must add extra page
-	if ((programSize % 4) > 0)
-		pages = pages + 1;
-
-	return pages;
-}
-
-void LinkedList::addProgram(string programName) {
-
-	//Check if head is null
-	//if it is, create "temporary" page
-	//Assign head values through this page and return
-	if (head == NULL) {
-		page *temp = new page;
-		temp->programName = programName;
-		temp->nextPage = NULL;
-		head = temp;
-		return;
-	}
-	//Create page to traverse through list, starting at head
-	page *current = head;
-
-	//Create new page
-	page *newPage = new page;
-	newPage->programName = programName;
-	newPage->nextPage = NULL;
-	//While the page after current has a name
-	//continue to traverse through list
-	while (current->nextPage != NULL) {
-		current = current->nextPage;
-	}
-	//When the page past current is uninitialized
-	//Use the new page created above to initialize it
-	current->nextPage = newPage;
-
-}
-
-int LinkedList::listLength() {
-	int count = 0;
-	page *current = head;
-	//while the page is initialized, increment the count variable
-	while (current != NULL) {
-		count++;
-		current = current->nextPage;
-	}
-	return count;
-}
-
-void LinkedList::insertBest() {
-	page *current = head;
-	int countUsed = 0;
-	int countFree = 0;
-	int freeAtEnd;
-	 
-	for (int i = 0; i < 32; i++) {
-		//if current page isn't NULL
-		if (current != NULL) {
-			countUsed++;
+		//Create new page
+		page *newPage = new page;
+		newPage->programName = programName;
+		newPage->nextPage = NULL;
+		//While the page after current has a name
+		//continue to traverse through list
+		while (current->nextPage != NULL) {
 			current = current->nextPage;
 		}
-		//if current page is NULL
-		else {
-			countFree++;
+		//When the page past current is uninitialized
+		//Use the new page created above to initialize it
+		current->nextPage = newPage;
+
+	}
+
+	int LinkedList::listLength() {
+		int count = 0;
+		page *current = head;
+		//while the page is initialized, increment the count variable
+		while (current != NULL) {
+			count++;
 			current = current->nextPage;
 		}
+		return count;
 	}
-}
 
-void displayMenu() {
-	cout << "\n";
-	cout << "1. Add Program" << endl;
-	cout << "2. Kill Program" << endl;
-	cout << "3. Fragmentation" << endl;
-	cout << "4. Print memory" << endl;
-	cout << "5. Exit" << endl;
-	cout << "\n";
-}
+	void LinkedList::insertBest(string programName) {
+		page *current = head;
+		int countUsed = 0;
+		int countFree = 0;
+		int freeAtEnd = 0;
+		int smallestFreeFrag = 40;
+
+		for (int i = 0; i < 32; i++) {
+			//if current page isn't NULL
+			if (current != NULL) {
+				countUsed++;
+				current = current->nextPage;
+			}
+			//if current page is NULL
+			else if (current == NULL) {
+				countFree++;
+				//if next page is NULL and free memory counter is smaller than the smallest fragment of free memory
+				//store that fragment as the smallest free fragment and mark the location using page *temp
+				if (current->nextPage != NULL) {
+					if (countFree < smallestFreeFrag) {
+						smallestFreeFrag = countFree;
+						countFree = 0;
+					}
+				}
+				current = current->nextPage;
+			}
+			//if page doesn't exist
+			else {
+				freeAtEnd++;
+			}
+		}
+		//if free memory at end
+		if (freeAtEnd < smallestFreeFrag) {
+			smallestFreeFrag = freeAtEnd;
+		}
+
+	}
+
+	void displayMenu() {
+		cout << "\n";
+		cout << "1. Add Program" << endl;
+		cout << "2. Kill Program" << endl;
+		cout << "3. Fragmentation" << endl;
+		cout << "4. Print memory" << endl;
+		cout << "5. Exit" << endl;
+		cout << "\n";
+	}
 
