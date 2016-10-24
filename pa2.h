908832d68ca1876e48ddb2158338e596b92ec4a8
tycanady <tycanady@gmail.com>
2016-10-23 22:20:16 -0400

    Update pa2.h

--- pa2.h
+++ pa2.h
@@ -6,23 +6,29 @@
 #include <string>
 using namespace std;
 
+//Each node is a page that has a name
 struct page {
-    string programName;
-    page *nextPage;
+	string programName;
+	page *nextPage;
 };
 
+//Linked List class
 class LinkedList {
 private:
-    page *head;
+	page *head;
 public:
-    LinkedList();
-    
-    void addProgram(string programName, int numPages);
-   // void displayList();
-    
-   // ~LinkedList();
+	//default linked list constructor
+	LinkedList();
+
+	//add a program to list
+	void addProgram(string programName);
+	//return length of linked list
+	int listLength();
+	void insertBest();
+
 };
 
+//Functions explained in pa2.cpp
 void bestFit();
 void worstFit();
 void displayMenu();
