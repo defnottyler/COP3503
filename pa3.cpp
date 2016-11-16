4608d51078cf44116de2513a4eaa5104f354e310
tycanady <tycanady@gmail.com>
2016-11-16 18:42:52 -0500

    Update pa3.cpp

--- pa3.cpp
+++ pa3.cpp
@@ -3,39 +3,48 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <deque>
 
 using namespace std;
 
 string getFileName();
-vector<string> getLines(string fileName);
+deque<string> getLines(string fileName);
+void printLines(deque<string> lines);
 
 int main() {
-
-	string fileName = getFileName();
-	vector<string> lines = getLines(fileName);
-
-	return 0;
+    
+    string fileName = getFileName();
+    deque<string> lines = getLines(fileName);
+    printLines(lines);
+    
+    return 0;
 }
 
 string getFileName() {
-
-	string fileName;
-
-	cout << "Enter text file name: ";
-	cin >> fileName;
-	cout << endl;
-
-	return fileName;
+    
+    string fileName;
+    
+    cout << "Enter text file name: ";
+    cin >> fileName;
+    cout << endl;
+    
+    return fileName;
 }
 
-vector<string> getLines(string fileName) {
-	vector<string> allLinesInText;
-	string line;
-	ifstream infile(fileName);
-	
-	while (getline(infile, line)) {
-		allLinesInText.push_back(line);
-	}
-
-	return allLinesInText;
+deque<string> getLines(string fileName) {
+    
+    deque<string> allLinesInText;
+    string line;
+    
+    ifstream infile(fileName);
+    
+    while (getline(infile, line)) {
+        allLinesInText.push_back(line);
+    }
+    
+    infile.close();
+    
+    return allLinesInText;
 }
+
+
